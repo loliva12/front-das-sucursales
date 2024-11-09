df31adfe9af38d9f7a67bdac1c474a897bab9633
loliva12 <olivadanielelucia@gmail.com>
2024-11-09 17:14:46 -0300

    agrego estilos

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { SucursalesComponent } from './app/sucursales/sucursales.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(SucursalesComponent).catch((err) => console.error(err));
+bootstrapApplication(SucursalesComponent, {
+  providers: [provideAnimationsAsync()]
+}).catch((err) => console.error(err));
