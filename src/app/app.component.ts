6e4428c9afd674d01ec6f619e065810f4f1197d1
loliva12 <olivadanielelucia@gmail.com>
2024-11-11 20:31:00 -0300

    add funcionalidad pero no funciona es para poder crear otra rama y no seguir rompiendo esta que medianamente anda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router'; // Importar Routes
 import { SucursalesComponent } from './sucursales/sucursales.component';
+import { ProductListComponent } from './product/product.component';
+
+const routes: Routes = [
+  { path: '', component: SucursalesComponent },
+  { path: 'productos', component: ProductListComponent }
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SucursalesComponent],
+  imports: [
+    CommonModule,
+    SucursalesComponent,
+    ProductListComponent,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sucursales-app';
+  title = 'mi-app';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { SucursalesComponent } from './app/sucursales/sucursales.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(SucursalesComponent, {
-  providers: [provideAnimationsAsync()]
-}).catch((err) => console.error(err));
+bootstrapApplication(SucursalesComponent).catch(err => console.error(err));
